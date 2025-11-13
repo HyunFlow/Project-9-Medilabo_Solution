2ab6a693b493ca2480f9b3cbfce70b664e55f809
Yonghyun LEE <yonghyunlee.dev@gmail.com>
2025-11-13 05:02:57 +0100

    refactor(patient-service): update Controller annotations and Patient Id field strategy from AUTO to IDENTITY

--- patient-service/src/main/java/com/openclassrooms/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/openclassrooms/patientservice/model/Patient.java
@@ -16,7 +16,7 @@ import lombok.Setter;
 @Entity(name = "patient_info")
 public class Patient {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name="first_name", nullable = false)
     private String firstName;
